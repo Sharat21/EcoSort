8383ab8f9e51f3fc4ef28a81b206fa62b68ac709
Harsh Patel <38195764+harshp14@users.noreply.github.com>
2025-04-04 21:11:16 -0400

    Updated classifcation file

--- classification.py
+++ classification.py
@@ -9,7 +9,8 @@ labels = {
     'plastic' : 2,
     'trash' : 3,
     'glass' : 4,
-    'paper' : 5
+    'paper' : 5,
+    'biodegradeble' : 6
 }
 
 label_list = list(labels.keys())
@@ -33,13 +34,13 @@ class ImageDataset(Dataset):
         label = self.images[image_path]
 
         # Open the image
-        image = Image.open(f"./dataset/{image_path}").convert("RGB")
+        image = Image.open(f"./dataset_segmented/{image_path}").convert("RGB")
 
         image = transform(image)
 
         return image, label
 
-def plot_heatmap(matrix):
+def plot_heatmap(matrix, name):
     """
     Plots a heatmap of the confusion matrix.
 
@@ -62,7 +63,7 @@ def plot_heatmap(matrix):
     plt.ylabel('True Label')
     plt.xticks(ticks=np.arange(len(label_list)), labels=label_list)
     plt.yticks(ticks=np.arange(len(label_list)), labels=label_list)
-    plt.savefig('confusion_matrix.png')
+    plt.savefig(name)
     plt.close()
 def create_model():
     """
@@ -106,7 +107,7 @@ def training_labels(train_size, test_size):
     from random import randint
     from torch import zeros
 
-    directory = './dataset'
+    directory = './dataset_segmented'
     training_images, validation_images, testing_images = {}, {}, {}
 
     for index, key in enumerate(list(labels.keys())):
@@ -117,16 +118,17 @@ def training_labels(train_size, test_size):
 
 
     for path in listdir(directory):
-        label = re.sub(r'\d+', '', path).replace(".jpg", "").replace(".png", "")
+
+        label = path.split('_')[0]
         set = randint(0, 100)
         if set < train_size:
-            training_images[path] = labels[label.replace("_", "")]
+            training_images[path] = labels[label]
 
         elif set > test_size:
-            testing_images[path] = labels[label.replace("_", "")]
+            testing_images[path] = labels[label]
 
         else:
-            validation_images[path] = labels[label.replace("_", "")]
+            validation_images[path] = labels[label]
 
     return training_images, validation_images, testing_images
 
@@ -150,7 +152,8 @@ def run_training_pipeline():
     import numpy as np
 
     global labels
-    confusion_matrix = torch.zeros(len(labels.keys()), len(labels.keys()))
+    confusion_matrix_original = torch.zeros(len(labels.keys()), len(labels.keys()))
+    confusion_matrix_fine_tuned = torch.zeros(len(labels.keys()), len(labels.keys()))
 
     with open('parameters.json') as f:
         parameters = loads(f.read())
@@ -159,9 +162,13 @@ def run_training_pipeline():
 
     print("Creating model")
     model = create_model()
+    model.load_state_dict(torch.load('classification_model.pth'))
     print("Creating training, validation, and testing sets")
     training_set, validation_set, testing_set = training_labels(parameters['train_size'], parameters['test_size'])
     model.to(device)
+
+    loss_function = nn.CrossEntropyLoss() #Applies softmax on its own, dont need to apply it to data
+    optim = Adam(model.parameters(), lr=parameters['learning_rate'])
 
     print("Creating training and validation data loaders")
     dataset = ImageDataset(training_set)
@@ -170,12 +177,10 @@ def run_training_pipeline():
     val_loader = DataLoader(dataset, batch_size=parameters['batch_size'], shuffle=True)
 
 
-    loss_function = nn.CrossEntropyLoss() #Applies softmax on its own, dont need to apply it to data
-    optim = Adam(model.parameters(), lr=parameters['learning_rate'])
-
     best_loss = float('inf')
     epochs_no_improve = 0
 
+    
     print("Creating training info")
     if not parameters['load_model']:
         training_info = {
@@ -243,21 +248,29 @@ def run_training_pipeline():
             if validation_loss < best_loss:
                 best_loss = validation_loss
                 epochs_no_improve = 0
-                torch.save(model.state_dict(), f'best_model_epoch_{epoch}.pth')
+                torch.save(model.state_dict(), f'classification_model_epoch_{epoch}.pth')
+                classification_model = model.state_dict()
             else:
                 epochs_no_improve += 1
 
             if epochs_no_improve >= parameters['patience']:
                 break
 
+    torch.save(classification_model, 'classification_model_fine_tuned.pth')
+
+
+    model.load_state_dict(torch.load('classification_model_fine_tuned.pth'))
+
     print("Creating testing data loader")
     dataset = ImageDataset(testing_set)
     test_loader = DataLoader(dataset, batch_size=parameters['batch_size'], shuffle=True)
 
-    # Testing loop
+    # Testing loop using fine tuned weights
+    print("Testing model with fine tuned weights")
     print("Evaluating model")
-    model.eval()
     testing_loss = 0
+    correct = 0
+    total = 0
     with torch.no_grad():
         for images, labels in test_loader:
 
@@ -273,21 +286,55 @@ def run_training_pipeline():
             for i in range(len(outputs)):
                 target_label = np.argmax(labels[i])  # Find the index of the column that contains the
                 predicted_label = np.argmax(outputs[i])  # Find the index of the column that contains the 1
-                confusion_matrix[target_label][predicted_label] += 1
-
+                confusion_matrix_fine_tuned[target_label][predicted_label] += 1
+                if target_label == predicted_label: correct += 1
+                total += 1
             del images
             del labels
             del outputs
             torch.cuda.empty_cache()
 
-    print(confusion_matrix)
-    plot_heatmap(confusion_matrix)
+    print(f"Accuracy: {(correct / total) * 100}")
+    print(confusion_matrix_fine_tuned)
+    plot_heatmap(confusion_matrix_fine_tuned, 'confusion_matrix_fine_tuned.png')
 
     print(f"Testing Loss: {testing_loss / len(test_loader)}")
-    training_info['test_loss'].append(testing_loss / len(test_loader))
 
-    with open("training_data.json", "w") as json_file:
-        dump(training_info, json_file, indent=4)
+    model.load_state_dict(torch.load('classification_model.pth'))
+    # Testing loop using original weights
+    print("Testing model with original weights")
+    print("Evaluating model")
+    testing_loss = 0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for images, labels in test_loader:
+
+            images = images.to(device)
+            labels = labels.to(device)
+            outputs = model(images)
+            loss = loss_function(outputs, labels)
+            testing_loss += loss.item()
+
+            outputs = outputs.cpu().numpy()
+            labels = labels.cpu().numpy()
+
+            for i in range(len(outputs)):
+                target_label = np.argmax(labels[i])  # Find the index of the column that contains the
+                predicted_label = np.argmax(outputs[i])  # Find the index of the column that contains the 1
+                confusion_matrix_original[target_label][predicted_label] += 1
+                if target_label == predicted_label: correct += 1
+                total += 1
+            del images
+            del labels
+            del outputs
+            torch.cuda.empty_cache()
+
+    print(f"Accuracy: {(correct / total) * 100}")
+    print(confusion_matrix_original)
+    plot_heatmap(confusion_matrix_original, 'confusion_matrix_original.png')
+
+    print(f"Testing Loss: {testing_loss / len(test_loader)}")
 
 
 def move_images():
@@ -305,7 +352,7 @@ def move_images():
     from random import randint
     from shutil import move
 
-    original = './dataset'
+    original = './dataset_segmented'
     new = './dataset_new'
 
     if not exists(new):
@@ -315,5 +362,31 @@ def move_images():
         if randint(0, 100) < 50:
             move(f'{original}/{image}', f'{new}/{image}')
 
+
+def inference(image):
+    """
+    Performs inference on a new image using the trained model.
+
+    This function loads the trained model, preprocesses the new image, and predicts its class.
+
+    Returns:
+        None
+    """
+    import torch
+    import numpy as np
+    import torchvision.transforms as transforms
+    model = create_model()
+    model.load_state_dict(torch.load('classification_model.pth'))
+    model.eval()
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    image = transform(image).unsqueeze(0).to(device)  # Apply transform, add batch dimension, and move to GPU
+
+    output = model(image)
+    output = output.cpu().detach().numpy()
+    prediction = np.argmax(output)
+    return label_list[prediction]
+
 #move_images() #Uncomment to move images to new dataset - Should only be done once. This allows for the model to be trained on a smaller dataset, and use the remaining images for fine tuning training afterwards using the segmentation model's output as a mask
-run_training_pipeline()
+#run_training_pipeline()
